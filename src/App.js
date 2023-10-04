95fd92e748e8b6dd118603e042a9232308f9b8bf
Erik Hunter <hunterej@ip-10-255-9-150.ec2.internal>
2023-10-04 14:00:03 -0500

    added cache refresh capabilites

--- src/App.js
+++ src/App.js
@@ -3,20 +3,47 @@ import './App.css';
 import Carousel from 'react-bootstrap/Carousel';
 import { fetchDrupalData } from './util/http';
 import { useEffect, useState} from "react";
+import { useQuery, useQueryClient} from 'react-query'
+
+
 
 function App() {
   const [imageURLs, setImageURLs] = useState()
-  useEffect(() => {
-    async function getDrupalData() {
-      const drupalData = await fetchDrupalData();
-      console.log(drupalData)
-      setImageURLs(drupalData)
-    }
-    getDrupalData();
-  }, []);
+  const queryClient = useQueryClient()
+  const { isLoading, isError, data, error } = useQuery('todos', fetchDrupalData)
+  setInterval(async() => {
+    const drupalData = await fetchDrupalData();
+    queryClient.setQueryData('todos', drupalData)
+  }, 
+    120000)
+  // useEffect(() => {
+  //   const clearCacheData = () => {
+  //     caches.keys().then((names) => {
+  //       names.forEach((name) => {
+  //         caches.delete(name);
+  //       });
+  //     });
+  //     console.log('Complete Cache Cleared')
+  //   };
+  //   async function getDrupalData() {
+  //     const drupalData = await fetchDrupalData();
+  //     console.log(drupalData)
+  //     setImageURLs(drupalData)
+  //   }
+  //   getDrupalData();
+  //   const intervalId = setInterval(() => {
+  //     clearCacheData()
+  //     // getDrupalData(); // Fetch data every 2 minutes
+  //     window.location.reload(1)
+  //   }, 120000);
+  //   return () => clearInterval(intervalId);
+  // }, []);
+
+  console.log(data)
+
   return (
     <div className="App">
-      {imageURLs && <Carousel interval={8000}>{imageURLs.map((imageURL) => (
+      {data && <Carousel interval={8000}>{data.map((imageURL) => (
            <Carousel.Item>
            <img
              style={{width: '100%', height: '100%'}}
